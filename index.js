bd387fbe5e21c0b5aa96be590ce679ce7c0b4fb6
pramodbnadgeri <nadageripramod03@gmail.com>
2022-09-12 17:45:18 +0530

    heroku pg port changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ pool.connect();
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: process.env.PORT || 3000,
     host: "0.0.0.0",
     // host: "localhost",
     routes: {
